35095392d3d84922f582070d9b055178493f0cb2
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-13 17:15:43 +0100

    created edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div class="container">
-    <form action="{{ route('admin.projects.update') }}" method="post">
+    <form action="{{ route('admin.projects.update', $project) }}" method="post">
         @csrf
         @method('PUT')
 
