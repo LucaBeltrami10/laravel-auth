ee12cffb6f8e015a48d722b732526f7bd03ffb48
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-14 09:42:18 +0100

    prepared model for soft deleting

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
     use HasFactory;
-
-    protected $guarded = [];
+    use SoftDeletes;
 
     protected $fillable = [
         'project_name',
